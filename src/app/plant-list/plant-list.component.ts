e9ed755bccaf93c8feed8cd32e6d55d8ced9202a
Anthony Cunningham <oxa547@gmail.com>
2020-04-08 20:01:53 -0500

    cmon

--- src/app/plant-list/plant-list.component.ts
+++ src/app/plant-list/plant-list.component.ts
@@ -2,7 +2,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 // rxjs
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { flatMap, takeWhile } from 'rxjs/operators';
 // models
 import { Plant } from '../models/plant/plant';
 // ngrx store
@@ -11,6 +12,7 @@ import * as fromApp from '../state/app.reducer';
 import * as appActions from '../state/app.actions';
 // services
 import { FirebaseService } from '../core/firebase.service';
+
 
 @Component({
   selector: 'plant-list',
@@ -25,15 +27,38 @@ export class PlantListComponent implements OnInit, OnDestroy {
   componentActive = true;
   defaultElevation = 2;
 
+  plantFileNames = [];
+  plantUrls = [];
+
   constructor(private store: Store<fromApp.AppState>,
               private router: Router,
               private firestore: FirebaseService) {
-                // this.plantImageUrl$ = this.firestore.getImageURL('fa7d40ca-5378-eeaf-0689-d29fb8349a7f');
-              }
+    // this.plantImageUrl$ = this.firestore.getImageURL('fa7d40ca-5378-eeaf-0689-d29fb8349a7f');
+  }
 
   ngOnInit(): void {
     this.plantsLoading$ = this.store.pipe(select(fromApp.getPlantsLoading));
     this.plants$ = this.store.pipe(select(fromApp.getPlants));
+
+    const aSubscription = this.plants$.subscribe( //takeWhile(),
+      {
+        next: (data) => {
+          if (data) {
+            data.forEach(x => {
+              this.plantFileNames.push(x.imageId);
+          });
+            this.plantFileNames.forEach(x => {
+              this.getImageURL(x);
+            });
+            //console.log(this.plantFileNames);
+            //console.log(this.plantUrls);
+        }
+        },
+        error(msg) {
+          console.log(msg);
+        }
+      }
+    );
   }
 
   navigate() {
@@ -41,7 +66,8 @@ export class PlantListComponent implements OnInit, OnDestroy {
   }
 
   getImageURL(imageId) {
-    this.plantImageUrl$ = this.firestore.getImageURL(imageId);
+    this.plantUrls.push(this.firestore.getImageURL(imageId).toPromise().then(x => x));
+    console.log(this.plantUrls);
   }
 
   ngOnDestroy() {
